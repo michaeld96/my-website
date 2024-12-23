bf7a83aaef1c53c7b17a427a8c2f0dbe80c0a5d4
Michael Dick <michaeldick000@gmail.com>
2024-12-23 15:24:12 -0500

    feat: added basic JWT token.

--- backend/src/API/Program.cs
+++ backend/src/API/Program.cs
@@ -1,11 +1,36 @@
+using System.Text;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+
+var environment = builder.Environment.EnvironmentName;
 
 builder.Services.AddDbContext<NotesContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"] ?? Environment.GetEnvironmentVariable("JWT_ISSUER"),
+            ValidAudience = jwtSettings["Audience"] ?? Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                jwtSettings["Secret"] ?? Environment.GetEnvironmentVariable("JWT_SECRET")
+                ?? throw new ArgumentNullException("JwtSettings:Secret or JWT_SECRET environment variable is not configured")
+            ))
+        };
+    });
 
 builder.Services.AddCors(options =>
 {
