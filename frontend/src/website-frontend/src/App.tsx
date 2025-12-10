24b60df5f3355d4c514b47435c79c663477e2ab1
Michael Dick <michaeldick000@gmail.com>
2025-12-09 21:50:59 -0500

    feat: have backend and frontend container working. frontend container cannot connect to the backend container tho.

--- frontend/src/website-frontend/src/App.tsx
+++ frontend/src/website-frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NewNote from './pages/NewNote';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import Editor from './pages/Editor';
@@ -15,7 +14,6 @@ const App: React.FC = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/new-note" element={ <NewNote/> }/>
                 <Route path="/admin" element={ <Login/> }/>
                 <Route path="/editor" element={ <Editor/> }/>
                 <Route path="/projects" element={ <Projects/> }/>
