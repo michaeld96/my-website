d96a971e3bead528d6cd889e7ccd7c3e3cbda8de
Michael Dick <michaeldick000@gmail.com>
2025-12-13 22:44:06 -0500

    feat: added remote backend

---
+++ infra/bootstrap/outputs.tf
@@ -0,0 +1,14 @@
+output "state_bucket_name" {
+  value = var.state_bucket_name
+  description = "S3 bucket name for Terraform state."
+}
+
+output "lock_table_name" {
+  value = var.lock_table_name
+  description = "DynamoDB table name for Terraform state locking."
+}
+
+output "aws_region" {
+    value = var.region
+    description = "Region where the state resources are located."
+}
---
+++ infra/bootstrap/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "state_bucket_name" {
+  type    = string
+  default = "michaeldickiostatebucket"
+}
+
+variable "lock_table_name" {
+  type = string
+  default = "michaeldickio_lock_table"
+}
