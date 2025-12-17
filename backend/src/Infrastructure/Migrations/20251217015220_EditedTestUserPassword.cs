8336614574e7be0d35f4e3d542360b73d12599b6
Michael Dick <michaeldick000@gmail.com>
2025-12-16 21:12:30 -0500

    fix: pushed a migration to have entity framework not crash

---
+++ backend/src/Infrastructure/Migrations/20251217015220_EditedTestUserPassword.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditedTestUserPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$12$58lIBD9ybdFElYutT9Bp9.MHDgInFH5BRpofb6XMOm31uqx5mKnMe");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "mikedick1");
+        }
+    }
+}
