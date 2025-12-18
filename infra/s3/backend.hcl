bdcf9ff83bdb902685d8ca08980ca176ef676cfe
Michael Dick <michaeldick000@gmail.com>
2025-12-17 21:26:08 -0500

    feat: added s3 bucket to store images.

---
+++ infra/s3/backend.hcl
@@ -0,0 +1,5 @@
+bucket          = "michaeldickiostatebucket"
+key             = "personal-website/s3/terraform.tfstate"
+region          = "us-east-2"
+encrypt         = true
+use_lockfile = true
---
+++ infra/s3/outputs.tf
@@ -0,0 +1,19 @@
+output "bucket_name" {
+  value = aws_s3_bucket.this.id
+  description = "The name of the bucket."
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.this.arn
+  description = "Application resource number for the bucket."
+}
+
+output "bucket_regional_domain_name" {
+  value = aws_s3_bucket.this.bucket_regional_domain_name
+  description = "The regional domain name of the bucket."
+}
+
+output "bucket_region" {
+  value = aws_s3_bucket.this.region
+  description = "Region of the bucket."
+}
